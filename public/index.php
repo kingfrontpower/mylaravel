3c62d9f7187403f27f721731f100f589f2286fda
power <kingfrontpower@gmail.com>
2016-07-20 11:28:42 +0800

    201160720 上午的課程

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Register The Auto Loader
+| Register The Auto Loader  
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
