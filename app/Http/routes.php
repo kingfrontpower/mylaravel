031fa022e8ea9d65f6acb48176f5214577679cd3
power <kingfrontpower@gmail.com>
2016-07-20 14:12:38 +0800

    定義不同的網址來實現要做的功能

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +14,26 @@
 |
 */
 
+
+
+/**
+ * 顯示所有任務
+ */
+ 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+});
+
+/**
+ * 接收表單來新增任務
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
 });
